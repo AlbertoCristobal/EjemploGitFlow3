04d4d68fb1a0b37cb33c539b0157714d2cf39687
Alberto Cristobal <albertcristo11@gmail.com>
2022-10-06 20:48:26 +0200

    añadido el codigo

--- src/com/ufv/ejemplo03/Main.java
+++ src/com/ufv/ejemplo03/Main.java
@@ -1,4 +1,45 @@
 package com.ufv.ejemplo03;
 
+
+import java.util.Scanner;
 public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); //escanea la sailda del teclado
+        Persona p = new Persona();
+    //    Perritos perro = new Perritos();
+
+        System.out.println("Escribe la raza del perro: ");
+    //    perro.setRaza(sc.nextLine());
+
+
+
+        System.out.println("Escribe tu nombre: ");
+        p.setNombre(sc.nextLine());
+        System.out.println(p.getNombre());
+
+        System.out.println("Escribe tu apellido: ");
+        p.setApellidos(sc.nextLine());
+        System.out.println(p.getApellidos());
+
+        System.out.println("Escribe tu dni sin letra: ");
+        p.setDniSinLetra(sc.nextInt());
+        System.out.println(p.getDniSinLetra());
+
+        System.out.println("Escribe tu edad: ");
+        p.setEdad(sc.nextInt());
+        System.out.println("tu edad es :" + p.getEdad());
+
+        //Mostramos los cambios
+        System.out.print("Buenos días: " + p.getNombre());
+        System.out.print(" " + p.getApellidos() + " con dni: " + p.getDniSinLetra());
+        System.out.println(" y edad: " + p.getEdad() );
+       // System.out.println(" Y tu perro es de raza: "+ perro.getRaza());
+
+
+
+
+
+    }
 }
+
--- src/com/ufv/ejemplo03/Persona.java
+++ src/com/ufv/ejemplo03/Persona.java
@@ -1,4 +1,50 @@
 package com.ufv.ejemplo03;
 
+
 public class Persona {
+    private String nombre;
+    private String apellidos;
+    private int dniSinLetra;
+    private int edad;
+
+
+    /**********************    Getter     **********************/
+
+
+    public String getNombre(){ //para poder acceder al valor de la variable en el main
+        return nombre;
+    }
+    public String getApellidos(){
+        return apellidos;
+    }
+    public int getDniSinLetra(){
+        return dniSinLetra;
+    }
+    public int getEdad(){
+        return edad;
+    }
+    public void setNombre(String nom){
+        this.nombre = nom;
+    }
+
+    /**********************    Setter     **********************/
+
+    public void setApellidos(String apell){
+        this.apellidos = apell;
+    }
+    public void setDniSinLetra(int dni){
+        this.dniSinLetra = dni;
+    }
+    public void setEdad(int ed){
+        if (ed >= 18){
+            System.out.println("Estás hecho un chaval");
+        } else {
+            System.out.println("Ya eres talludito");
+        }
+        this.edad = ed;
+    }
+
+
+
 }
+
